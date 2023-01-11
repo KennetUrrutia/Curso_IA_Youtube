03e44d8913a94657dcf2ae681ed4635140649a0f
Kenneth De Leon <Kenneth@somosmangoverde.com>
2023-01-10 21:40:01 -0600

    POO

--- POO/main.py
+++ POO/main.py
@@ -1,8 +1,49 @@
 from Empleado import Empleado
 from Cliente import Cliente
 
-emp = Empleado('Kennet', 'Urrutia', '12314241', '45959827', '1000')
-cli = Cliente('Kennet', 'Urrutia', '12314241', '45959827', 'VIP')
 
-print('Empleado:\n',emp, '\n')
-print('Cliente:\n',cli, '\n')
+def cargar():
+    salir = True
+    
+    while salir:
+        validador = True
+        validador2 = True
+    
+        while validador:
+            respuesta = input('Va a ingresar un empleado? >')
+            if (respuesta == 'No' or respuesta == 'Si' or respuesta == 'no' or respuesta == 'si'):
+                validador = False
+            else:
+                print('Respuesta invalida')
+    
+        nombre = input('Ingrese el nombre > ')
+        apellido = input('Ingrese el Apellido > ')
+        dni = input('Ingrese el DNI > ')
+        telefono = input('Ingrese el teléfono > ')
+
+        if respuesta == 'si' or respuesta == 'Si':
+            salario = input('Ingrese su salario > ')
+            emp = Empleado(nombre, apellido, dni, telefono, int(salario))
+            personas.append(emp)
+        else:
+            tipo = input('Ingrese el tipo de cliente > ')
+            cli = Empleado(nombre, apellido, dni, telefono, tipo)
+            personas.append(cli)
+
+        while validador2:
+            respuesta2 = input('Desea seguir agregando personas? > ')
+            if (respuesta2 == 'No' or respuesta2 == 'Si' or respuesta2 == 'no' or respuesta2 == 'si'):
+                validador2 = False
+                if respuesta2 == 'No' or respuesta2 == 'no':
+                    salir = False
+            else:
+                print('Respuesta invalida')
+
+
+personas = []
+cargar()
+for persona in personas:
+    print(persona)
+
+# print('Empleado:\n',emp, '\n')
+# print('Cliente:\n',cli, '\n')
